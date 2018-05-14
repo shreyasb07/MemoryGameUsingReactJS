72c2f1c41f6c94fc03972dd102ea4f8f2939554b
Shreyas Bhosale <shreyas.tesla@gmail.com>
2018-05-13 21:29:53 -0700

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Card.css'
+
+const Card = (props) => {
+	let style = {};
+	if (props.showing) {
+		style.backgroundColor = props.backgroundColor;
+	}
+
+	return (
+		<div 
+			onClick = {props.onClick} 
+			className = "card-container" 
+			style={style} 
+		/>
+	);
+};
+
+Card.PropTypes = {
+	showing:PropTypes.bool.isRequired,
+	backgroundColor: PropTypes.string.isRequired,
+	onClick : PropTypes.func.isRequired
+};
+
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MemoryGame from './MemoryGame';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+	<MemoryGame />,
+	 document.getElementById('root')
+	 );
+registerServiceWorker();
